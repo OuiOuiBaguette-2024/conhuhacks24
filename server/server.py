a276cde9700737d6ff1aaecb912be7c73284d2e2
Alexandre Ondet <61289458+AlexandreOndet@users.noreply.github.com>
2024-01-20 18:44:08 -0500

    added env and request to stm API (#4)
    * added env and request to stm API
    
    * solved cicd
    
    ---------
    
    Co-authored-by: Mehdi Benouhoud <mehdi.benouhoud@polymtl.ca>

--- server/server.py
+++ server/server.py
@@ -1,14 +1,22 @@
 from typing import Union
 
 from fastapi import FastAPI
+import requests
+import os
 
 app = FastAPI()
+apiKey = os.environ.get('apiKey')
 
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
+@app.get("/api")
+def read_api():
+    print(apiKey)
+    r=requests.get("https://api.stm.info/pub/od/gtfs-rt/ic/v2/vehiclePositions/", headers={"accept": "application/x-protobuf", "apiKey": apiKey})
+    return r.status_code
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
