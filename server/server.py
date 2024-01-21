30858b4f2b5e8120088256b869ed5f843ed347f0
Alexandre Ondet <alexandre.ondet63@gmail.com>
2024-01-20 20:51:06 -0500

    added gtfs data and module

--- server/server.py
+++ server/server.py
@@ -1,8 +1,10 @@
+from pathlib import Path
 from typing import Union
 
 from fastapi import FastAPI
 import requests
 import os
+import gtfs_kit as gk
 
 app = FastAPI()
 apiKey = os.environ.get('apiKey')
@@ -18,6 +20,14 @@ def read_api():
     r=requests.get("https://api.stm.info/pub/od/gtfs-rt/ic/v2/vehiclePositions/", headers={"accept": "application/x-protobuf", "apiKey": apiKey})
     return r.status_code
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/api/metro")
+@app.get("/api/metro/{route_id}/{direction_id}/{timestamp}")
+def get_metro_coordinates(route_id=1, direction_id=0,timestamp=0):
+    path = Path('data/gtfs_stm.zip')
+
+    feed = gk.read_feed(path, dist_units='km')
+
+    return feed.validate()
+
+    
+    
