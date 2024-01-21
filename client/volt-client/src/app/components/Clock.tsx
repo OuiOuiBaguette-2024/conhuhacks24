29c602e35bc42897336ea5925129e79d9da1ab55
Jonathan Bouchard <dev.drakota@gmail.com>
2024-01-20 21:37:39 -0500

    Added map (#13)

---
+++ client/volt-client/src/app/components/Clock.tsx
@@ -0,0 +1,9 @@
+const Clock = () => {
+  return (
+    <div className="bg-main py-5 px-20 rounded-lg">
+      <div className="font-bold text-lg">9:05 pm</div>
+    </div>
+  );
+};
+
+export default Clock;
--- client/volt-client/tailwind.config.ts
+++ client/volt-client/tailwind.config.ts
@@ -7,6 +7,9 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    colors: {
+      main: "#181c1f"
+    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
